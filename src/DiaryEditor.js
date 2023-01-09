ab6e2652c14622b2b2030494a9d82a6c0615c2fa
naeunk1m <107620483+naeunk1m@users.noreply.github.com>
2023-01-09 21:36:47 +0900

    2023-01-09 [수정중]

---
+++ src/DiaryEditor.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const DiaryEditor = () => {
+
+    const [author, setAuthor] = useState("");
+    const [content, setContent] = useState("");
+
+    return (
+        <div className="DiaryEditor">
+            <h2>오늘의 일기</h2>
+            <div>
+                <input
+                    name="author"
+                    value={author}
+                    onChange={(e) => {
+                        setAuthor(e.target.value);
+                    }} />
+            </div>
+            <div>
+                <textarea
+                    value={content}
+                    onChange={(e) => {
+                        setContent(e.target.value);
+                    }} />
+            </div>
+        </div>
+    );
+};
+export default DiaryEditor
